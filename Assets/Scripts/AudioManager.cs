87a0391b3b2a57ff727185f71f15d1134948e98d
Kerwin Yen <kevinyenhaha@gmail.com>
2022-09-25 03:42:39 +1000

    v3.5 Play BGM_normal after the end of intro music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip BGM;
+    AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = BGM;
+            audioSource.Play();
+        }
+    }
+}
