b7fd691411b4fedd562e646e1b455ed133af2bd8
Kerwin Yen <kevinyenhaha@gmail.com>
2022-10-30 23:35:26 +1100

    v9.2
    - Added move function

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PacStudentController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float walkSpeed = 2.0f;
+
     void Start()
     {
         
@@ -13,6 +15,33 @@ public class PacStudentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        IdentifyInput();
     }
+    void IdentifyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Movement (Vector2.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            Movement(Vector2.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Movement(Vector2.right);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Movement(Vector2.down);
+        }
+        
+
+
+    }
+    void Movement(Vector2 k) {
+        transform.localPosition += (Vector3) (k * walkSpeed) * Time.deltaTime;
+
+    }
+
 }
